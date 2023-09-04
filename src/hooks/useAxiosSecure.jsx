b688de3fa2296025cf9091bbef4117764db75ca7
nazmussaadatcse <nazmus.bd97@gmail.com>
2023-09-05 03:22:09 +0600

    useAxiosSecure Custom hook

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,54 @@
+import { useContext } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
+
+const useAxiosSecure = () => {
+    const { logOut } = useContext(AuthContext); // Replace with your actual AuthContext
+    const navigate = useNavigate();
+
+    // Create an Axios instance with a base URL
+    const axiosSecure = axios.create({
+        baseURL: 'http://localhost:5000', // Replace with your API base URL
+    });
+
+    // Add a request interceptor to inject the authorization header
+    axiosSecure.interceptors.request.use(
+        (config) => {
+            // Get the token from local storage
+            const token = localStorage.getItem('access-token');
+
+            // Add the token to the request headers
+            if (token) {
+                config.headers.Authorization = `Bearer ${token}`;
+            }
+
+            return config;
+        },
+        (error) => {
+            return Promise.reject(error);
+        }
+    );
+
+    // Add a response interceptor to handle 401 and 403 responses
+    axiosSecure.interceptors.response.use(
+        (response) => {
+            return response;
+        },
+        async (error) => {
+            if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+                // Handle 401 and 403 responses here
+                // Log out the user and redirect to the login page
+                await logOut();
+                navigate('/login'); // Redirect to your login page
+            }
+
+            return Promise.reject(error);
+        }
+    );
+    // [logOut, navigate, axiosSecure]
+
+    return [axiosSecure]; // Return the Axios instance
+};
+
+export default useAxiosSecure;
--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,23 +1,32 @@
 import { useQuery } from '@tanstack/react-query'
 import { useContext } from 'react';
+import useAxiosSecure from './useAxiosSecure';
 import { AuthContext } from '../providers/AuthProvider';
 // tanstack query 
 const useCart = () => {
     const { user } = useContext(AuthContext);
-    const token = localStorage.getItem('access-token')
+    const [axiosSecure] = useAxiosSecure();
+    // const token = localStorage.getItem('access-token')
 
     const { refetch, data: cart = [] } = useQuery({
         queryKey: ['carts', user?.email],
         queryFn: async () => {
-            const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`,{
-                headers:{
-                    authorization: `bearer ${token}`
-                }
-            })
-            return response.json();
+            if (!user) {
+                return [];
+            }
+            const response = await axiosSecure(`/carts?email=${user?.email}`)
+            return response.data;
         },
+        // queryFn: async () => {
+        //     const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`,{
+        //         headers:{
+        //             authorization: `bearer ${token}`
+        //         }
+        //     })
+        //     return response.json();
+        // },
     })
-    return [cart, refetch]
+    return [cart, refetch, useAxiosSecure]
 
 };
 
