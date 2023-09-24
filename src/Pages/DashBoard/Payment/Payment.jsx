c360c0d9cec788d0d1b98fc78a01bd8da4b4ca1b
nazmussaadatcse <nazmus.bd97@gmail.com>
2023-09-24 21:46:37 +0600

    stripe install and basic setup

--- src/Pages/DashBoard/Payment/Payment.jsx
+++ src/Pages/DashBoard/Payment/Payment.jsx
@@ -1,11 +1,21 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import CheckoutForm from "./CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
 
+
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK); //TODO: provide pk
 
 const Payment = () => {
     return (
         <div className="w-full p-8">
             <SectionTitle heading={'Payment'} subHeading={'Please Pay Now'}></SectionTitle>
             <h3>Payment</h3>
+
+            <Elements stripe={stripePromise}>
+                <CheckoutForm></CheckoutForm>
+            </Elements>
+
         </div>
     );
 };
